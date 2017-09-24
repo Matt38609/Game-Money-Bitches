dda8a711ddc3cba68d9b734763aafe3bb608e066
JagSwag2014 <pacman1225@gmail.com>
2017-09-24 18:12:08 -0500

    add all the shit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public int moveSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Translate(Vector3.up * Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+		transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime);
+	}
+}
